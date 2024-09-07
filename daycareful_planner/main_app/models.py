a253d4c022559fafbdcdfb6fd02e87781969603a
SkyActually <jimmiesmith1222@gmail.com>
2024-09-06 22:52:19 -0500

    made basic models

--- daycareful_planner/main_app/models.py
+++ daycareful_planner/main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class ParentInfo(models.Model):
+    name = models.CharField(max_length=50)
+    kidsName = models.CharField(max_length=50)
+    paid = models.IntegerField()
+    attendance = models.IntegerField()
+
+class ChildInfo(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    allergies = models.TextField(max_length=250)
+
+class EmployeeInfo(models.Model):
+    name = models.CharField(max_length=50)
+    hours = models.IntegerField()
+    attendance = models.IntegerField()
+    pay = models.IntegerField()
+    position = models.CharField(max_length=50)

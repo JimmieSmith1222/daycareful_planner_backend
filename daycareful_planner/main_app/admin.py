79cff8f88d658a5234757c251a5dd3322bced782
SkyActually <jimmiesmith1222@gmail.com>
2024-09-10 17:41:24 -0500

    added files and tested for basic Django application.

--- daycareful_planner/main_app/admin.py
+++ daycareful_planner/main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ParentInfo, ChildInfo, EmployeeInfo
 
-# Register your models here.
+admin.site.register(ParentInfo)
+admin.site.register(ChildInfo)
+admin.site.register(EmployeeInfo)
--- daycareful_planner/main_app/views.py
+++ daycareful_planner/main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import ParentInfo, ChildInfo, EmployeeInfo
 
-# Create your views here.
+def index(request):
+    parents = ParentInfo.objects.all()
+    children = ChildInfo.objects.all()
+    employees = EmployeeInfo.objects.all()
+    return render(request, 'index.html', {
+        'parents': parents,
+        'children': children,
+        'employees': employees
+    })

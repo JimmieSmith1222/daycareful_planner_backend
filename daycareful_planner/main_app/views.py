63dc77bfdaa46dfbd0eb617dc4727c82ea860660
SkyActually <jimmiesmith1222@gmail.com>
2024-09-27 15:27:33 -0500

    ok all crud functions work for each user

--- daycareful_planner/main_app/views.py
+++ daycareful_planner/main_app/views.py
@@ -84,7 +84,9 @@ def child_edit(request, id):
     if request.method == "POST":
         form = ChildForm(request.POST, instance=child)
         if form.is_valid():
-            child = form.save()
+            child_info = form.save(commit=False)
+            child_info.user = child.user
+            child_info.save()
             return redirect('child_detail', id=child.id)
     else:
         form = ChildForm(instance=child)
@@ -113,7 +115,9 @@ def employee_create(request):
     if request.method == "POST":
         form = EmployeeForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            employee_info = form.save(commit=False)
+            employee_info.user = request.user
+            employee_info.save()
             return redirect('employee_list')
     else:
         form = EmployeeForm()
@@ -125,7 +129,9 @@ def employee_edit(request, id):
     if request.method == "POST":
         form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
-            employee = form.save()
+            employee_info = form.save(commit=False)
+            employee_info.user = employee.user
+            employee_info.save()
             return redirect('employee_detail', id=employee.id)
     else:
         form = EmployeeForm(instance=employee)

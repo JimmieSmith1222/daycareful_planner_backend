54e9b460eb9fd25479854c19d816b2d6b0bb334a
SkyActually <jimmiesmith1222@gmail.com>
2024-10-02 01:17:19 -0500

    made a lot of CSS changes.

--- daycareful_planner/daycareful_planner/settings.py
+++ daycareful_planner/daycareful_planner/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Add login and logout redirect URLs
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- daycareful_planner/daycareful_planner/urls.py
+++ daycareful_planner/daycareful_planner/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from main_app.views import SignUpView
+from main_app.views import SignUpView, custom_logout
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', custom_logout, name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
 
